05533769db1b5d68848c10054c519cebaa4c7ad8
LoreSchaeffer <lm.lorenzo@outlook.it>
2024-05-07 11:30:07 +0200

    Rewriting

---
+++ src/main/java/it/multicoredev/computer/util/gates/DualInputGate.java
@@ -0,0 +1,6 @@
+package it.multicoredev.computer.util.gates;
+
+public interface DualInputGate {
+
+    byte out(final byte a, final byte b);
+}
---
+++ src/main/java/it/multicoredev/computer/util/gates/SingleInputGate.java
@@ -0,0 +1,6 @@
+package it.multicoredev.computer.util.gates;
+
+public interface SingleInputGate {
+
+    byte out(final byte input);
+}
